9a51cc95684d73beb7e802d35f91f28d94822394
Jonjo Wadwa <57592068+jonjo156@users.noreply.github.com>
2023-04-19 17:18:16 +0100

    Create session_1.dart

---
+++ session_1.dart
@@ -0,0 +1,46 @@
+void main() {
+  
+  var json = {"no_of_comments":1,"sentiment":"Bearish","sentiment_score":-0.258,"ticker":"UAE"};
+  
+  var weather = 'Raining';
+  
+  
+  if (weather == 'Raining'){
+    print('Go get coat');
+  } else {
+    print('Continue');
+  }
+  
+  
+}
+
+//Data types
+
+
+
+// String
+
+String weather = 'Raning';
+
+
+//int
+int number = 45;
+
+
+//double
+double decimalNumber = 454.147548;
+
+
+//boolean
+bool yesOrNo = true;
+
+
+//Lists
+List<int> thisList =[1,2,3];
+
+
+//Map
+Map<String, dynamic> thisMap = {
+  'London': 'Raining',
+  'DavesAge': 70
+};
